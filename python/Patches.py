8b3a9354e76445973365e28a2f41450137456ce5
Hong-King <1291651621@qq.com>
2023-07-03 01:00:34 +0100

    update

--- python/Patches.py
+++ python/Patches.py
@@ -1,34 +1,20 @@
+## build-in functions
 import numpy as np
 import os
 from PIL import Image
 import sys
 
-# load data
-
-def load_im(path, endpoint = None):
-    i = 0
-    images = np.zeros(shape=(512,512,3,30))
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            # folder_name = os.path.basename(os.path.dirname(file_path))
-            # img_name = os.path.basename(file_path)
-            img = Image.open(file_path)
-            img = img.resize((512,512))
-
-            images[:,:,:,i] = np.array(img)
-            i = i + 1
-
-            # print(folder_name, img_name, img.size)
-    return images
-        
+## make wheels
+from read_im import read_im
 
 path =  "D:\CV\Codes\ViT\data_test"
 
-images = load_im(path)
+images = read_im(path)
 
-img = np.zeros(shape=(512,512,3))
-img = images[:,:,:,29]
+print(len(images))
+
+# img = np.zeros(shape=(512,512,3))
+img = images[29]
 
 
 
--- python/read_im.py
+++ python/read_im.py
@@ -1,15 +1,28 @@
+import numpy as np
 import os
-#import cv2
 from PIL import Image
 
 def read_im(path, endpoint = None):
+    images = []
     for root, dirs, files in os.walk(path):
         for file in files:
             file_path = os.path.join(root, file)
-            folder_name = os.path.basename(os.path.dirname(file_path))
-            img_name = os.path.basename(file_path)
             img = Image.open(file_path)
-            print(folder_name, img_name, img.size)
+            img = img.resize((512,512))
 
-path =  "D:\CV\Codes\ViT\data_test"
-read_im(path)
+            images.append(np.array(img))
+
+    return images
+
+
+# def read_im(path, endpoint = None):
+#     for root, dirs, files in os.walk(path):
+#         for file in files:
+#             file_path = os.path.join(root, file)
+#             folder_name = os.path.basename(os.path.dirname(file_path))
+#             img_name = os.path.basename(file_path)
+#             img = Image.open(file_path)
+#             print(folder_name, img_name, img.size)
+
+# path =  "D:\CV\Codes\ViT\data_test"
+# read_im(path)
